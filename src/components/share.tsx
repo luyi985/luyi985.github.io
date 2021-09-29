1deda1e6d99f7586985b333233f6076e16f88635
Yi Lu <luyi985@gmail.com>
2021-09-29 12:30:13 +1000

    with windo pop

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -15,6 +15,10 @@ export const Share: React.FunctionComponent<{
   const shareUrl = url || location.href;
   const shareTitle = title || get(match, ["params", "name"], "");
   const shareSummary = text || "";
+  const w = 400;
+  const h = 300;
+  const y = window.top.outerHeight / 2 + window.top.screenY - h / 2;
+  const x = window.top.outerWidth / 2 + window.top.screenX - w / 2;
   return (
     <Layout
       template={"repeat(3, 30px)"}
@@ -40,7 +44,17 @@ export const Share: React.FunctionComponent<{
       <Modal
         title="Share on LinkedIn"
         button={({ setOpen }) => (
-          <IconBtn onClick={() => setOpen(true)} width={30} height={30}>
+          <IconBtn
+            onClick={
+              //() => setOpen(true)} width={30} height={30}
+              () =>
+                window.open(
+                  `https://www.linkedin.com/shareArticle?mini=true&url=${"https://luyi985.github.io/"}&title=${shareTitle}&summary=${shareSummary}`,
+                  shareTitle,
+                  `menubar=yes,status=yes,width=${w},height=${h},left=${x},top=${y}`
+                )
+            }
+          >
             <LinkedIn />
           </IconBtn>
         )}
